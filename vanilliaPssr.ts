a605238d74b653b6d405e1d3c98f63aa20b2005b
Antonin Carlin <antonin.carlin@icloud.com>
2020-11-09 18:50:44 +0100

    test without oak

---
+++ vanilliaPssr.ts
@@ -0,0 +1,53 @@
+import { serve } from "https://deno.land/std@0.76.0/http/server.ts";
+import type { ServerRequest } from "https://deno.land/std@0.76.0/http/server.ts";
+import { encode } from "https://deno.land/std@0.73.0/encoding/utf8.ts";
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+
+for await (const req of s) {
+  $progressiveRendering(req);
+}
+
+async function $progressiveRendering(req: ServerRequest) {
+  const xmlChunks = [
+    "<html><body>",
+    "<h1>Hello world 1</h1>",
+    "<h2>Hello world 2</h2>",
+    "<h3>Hello world 3</h3>",
+    "</body></html>",
+  ];
+
+  const reader: Deno.Reader = {
+    async read(p: Uint8Array) {
+      if (!xmlChunks.length) {
+        return null;
+      } else {
+        const chunk = encode(xmlChunks.shift());
+        //Should sent a chunk each 100ms
+        await sleep(100);
+        p.set(chunk);
+        return chunk.length;
+      }
+    },
+  };
+
+  async function sleep(ms: number) {
+    return new Promise((resolve) => {
+      const waitT = setTimeout(() => {
+        clearTimeout(waitT);
+        resolve();
+      }, ms);
+    });
+  }
+  req.respond({
+    body: reader,
+    status: 200,
+    headers: new Headers({
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/html; charset=utf-8",
+      Connection: "keep-alive",
+      "X-Content-Type-Options": "nosniff",
+      "Transfer-Encoding": "chunked",
+    }),
+  });
+}
